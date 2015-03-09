dfb111506e6fd02f2e92ac1556d3e3cfa5288eee
Simon Walker <s.r.walker101@googlemail.com>
2015-03-09 09:27:47 +0000

    Add basic installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='srw',
+        author='Simon Walker',
+        author_email='s.walker.2@warwick.ac.uk',
+        version='0.0.1',
+        packages=find_packages(),
+        )
+
